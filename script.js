69678cc501b34dfeb2b9ca78624ab4231f73cee9
HordynskyiLev <117231069+HordynskyiLev@users.noreply.github.com>
2022-12-03 23:59:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Task 1
+let city = 'Київ! ';
+city = 'Львів! ';
+let address = city;
+alert(address)
+// Task 2
+let n = 10
+let result = 10 / 3
+console.log(result)
+// Task 3
+const a = 5.25
+const b = 3.50
+const c = 4
+const d = 6
+const e = a * c
+const f = b * d
+document.write(e + f)
